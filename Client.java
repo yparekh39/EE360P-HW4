f0b291253741e7d36f4a4d6c38b1faf0458aa225
Yash Parekh <yparekh39@gmail.com>
2017-03-20 15:31:12 -0500

    copied client code

--- Client.java
+++ Client.java
@@ -16,15 +16,46 @@ public class Client {
       if (tokens[0].equals("purchase")) {
         // TODO: send appropriate command to the server and display the
         // appropriate responses form the server
+        out.println("purchase " + tokens[1] + " " + tokens[2] + " " + tokens[3]);
+          while((line = in.readLine()) != null){
+            if(line.equals("END")){
+              break;
+            }
+            System.out.println(line);
+          }
+
       } else if (tokens[0].equals("cancel")) {
         // TODO: send appropriate command to the server and display the
         // appropriate responses form the server
+          out.println("cancel " + tokens[1]);
+          while((line = in.readLine()) != null){
+            if(line.equals("END")){
+              break;
+            }
+            System.out.println(line);
+          }
+
       } else if (tokens[0].equals("search")) {
         // TODO: send appropriate command to the server and display the
         // appropriate responses form the server
+          out.println("search " + tokens[1]);
+          while((line = in.readLine()) != null){
+            if(line.equals("END")){
+              break;
+            }
+            System.out.println(line);
+          }
+
       } else if (tokens[0].equals("list")) {
         // TODO: send appropriate command to the server and display the
         // appropriate responses form the server
+          out.println("list");
+          while((line = in.readLine()) != null){
+            if(line.equals("END")){
+              break;
+            }
+            System.out.println(line);
+          }
       } else {
         System.out.println("ERROR: No such command");
       }
