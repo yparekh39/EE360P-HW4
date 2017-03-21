7de1c5d6ff1baaa0f798c7755b1e4d440d346163
Yash Parekh <yparekh39@gmail.com>
2017-03-21 15:37:00 -0500

    Connected to sockets

--- Client.java
+++ Client.java
@@ -1,6 +1,8 @@
 import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.List;
+import java.io.*;
+import java.net.*;
 
 public class Client {
   public static void main (String[] args) {
@@ -16,57 +18,76 @@ public class Client {
     }
 
     while(sc.hasNextLine()) {
-      String cmd = sc.nextLine();
-      String[] tokens = cmd.split(" ");
-
-      if (tokens[0].equals("purchase")) {
-        // TODO: send appropriate command to the server and display the
-        // appropriate responses form the server
-        out.println("purchase " + tokens[1] + " " + tokens[2] + " " + tokens[3]);
-          while((line = in.readLine()) != null){
-            if(line.equals("END")){
-              break;
-            }
-            System.out.println(line);
+      try(
+        Socket socket = new Socket();
+      ){
+        for(int i = 0; i < servers.size(); i++){
+          try{
+            socket.connect(new InetSocketAddress(servers.get(i).ipAddr, servers.get(i).port), 100);
+          } catch(Exception e){
+            servers.remove(i);
+            continue;
           }
+          break;
+        }
+        
+        PrintWriter out =
+          new PrintWriter(socket.getOutputStream(), true);
+        BufferedReader in =
+          new BufferedReader(
+            new InputStreamReader(socket.getInputStream()));
 
-      } else if (tokens[0].equals("cancel")) {
-        // TODO: send appropriate command to the server and display the
-        // appropriate responses form the server
-          out.println("cancel " + tokens[1]);
-          while((line = in.readLine()) != null){
-            if(line.equals("END")){
-              break;
-            }
-            System.out.println(line);
-          }
+        String cmd = sc.nextLine();
+        String[] tokens = cmd.split(" ");
 
-      } else if (tokens[0].equals("search")) {
-        // TODO: send appropriate command to the server and display the
-        // appropriate responses form the server
-          out.println("search " + tokens[1]);
-          while((line = in.readLine()) != null){
-            if(line.equals("END")){
-              break;
+        if (tokens[0].equals("purchase")) {
+          // TODO: send appropriate command to the server and display the
+          // appropriate responses form the server
+          out.println("purchase " + tokens[1] + " " + tokens[2] + " " + tokens[3]);
+            while((line = in.readLine()) != null){
+              if(line.equals("END")){
+                break;
+              }
+              System.out.println(line);
             }
-            System.out.println(line);
-          }
 
-      } else if (tokens[0].equals("list")) {
-        // TODO: send appropriate command to the server and display the
-        // appropriate responses form the server
-          out.println("list");
-          while((line = in.readLine()) != null){
-            if(line.equals("END")){
-              break;
+        } else if (tokens[0].equals("cancel")) {
+          // TODO: send appropriate command to the server and display the
+          // appropriate responses form the server
+            out.println("cancel " + tokens[1]);
+            while((line = in.readLine()) != null){
+              if(line.equals("END")){
+                break;
+              }
+              System.out.println(line);
             }
-            System.out.println(line);
-          }
-      } else {
-        System.out.println("ERROR: No such command");
-      }
+
+        } else if (tokens[0].equals("search")) {
+          // TODO: send appropriate command to the server and display the
+          // appropriate responses form the server
+            out.println("search " + tokens[1]);
+            while((line = in.readLine()) != null){
+              if(line.equals("END")){
+                break;
+              }
+              System.out.println(line);
+            }
+
+        } else if (tokens[0].equals("list")) {
+          // TODO: send appropriate command to the server and display the
+          // appropriate responses form the server
+            out.println("list");
+            while((line = in.readLine()) != null){
+              if(line.equals("END")){
+                break;
+              }
+              System.out.println(line);
+            }
+        } else {
+          System.out.println("ERROR: No such command");
+        }
+      } catch(Exception e){ }
     }
-  }
 
-  
+  }
 }
